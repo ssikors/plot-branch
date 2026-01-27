5d5f2adc923c44d83602faf1bc1c09aba5c7ed4e
ssikors <stanislaw.s.2000@gmail.com>
2026-01-27 21:41:08 +0100

    fix

--- plot-branch-client/src/components/nodes/ChoiceNode.tsx
+++ plot-branch-client/src/components/nodes/ChoiceNode.tsx
@@ -7,7 +7,7 @@ type ChoiceNode = Node;
 export function ChoiceNode({ data }: NodeProps<ChoiceNode>) {
   return (
     <div className="rounded border bg-orange-300 p-4 pt-5 shadow">
-      <div className="font-bold mb-1 text-center">Start</div>
+      <div className="font-bold mb-1 text-center">Choice</div>
       <Handle type="target" position={Position.Top} />
       <Handle type="source" id="left"  position={Position.Left} />
       <Handle type="source" id="right"  position={Position.Right} />
