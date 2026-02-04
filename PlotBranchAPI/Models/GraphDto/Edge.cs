eabffca05dd48198e83e8a6126e8fafc655fa2ed
ssikors <stanislaw.s.2000@gmail.com>
2026-02-04 23:15:47 +0100

    fix

--- PlotBranchAPI/Models/GraphDto/Edge.cs
+++ PlotBranchAPI/Models/GraphDto/Edge.cs
@@ -6,6 +6,6 @@
         public string Source { get; set; }
         public string Target { get; set; }
 
-        public string SourceHandle { get; set; }
+        public string? SourceHandle { get; set; }
     }
 }
